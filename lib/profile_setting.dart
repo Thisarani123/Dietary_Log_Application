fda18e6d81e50a3b2cdcdc9ebb0e41db2cdee1f8
Thisarani123 <thisaranikaushalya5@gmail.com>
2025-02-17 15:14:49 +0530

    Added profile page

---
+++ lib/profile_setting.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class profilesetting extends StatefulWidget {
+  const profilesetting({super.key});
+
+  @override
+  State<profilesetting> createState() => _profilesettingState();
+}
+
+class _profilesettingState extends State<profilesetting> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
